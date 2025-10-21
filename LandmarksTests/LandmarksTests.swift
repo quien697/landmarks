32385e0312ae0e462ee8b0b0b8c7ae6dd0da9880
Quien <quien697@gmail.com>
2025-10-21 22:17:02 +0800

    Initial Commit

---
+++ LandmarksTests/LandmarksTests.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksTests.swift
+//  LandmarksTests
+//
+//  Created by Quien on 2025-10-21.
+//
+
+import Testing
+@testable import Landmarks
+
+struct LandmarksTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
