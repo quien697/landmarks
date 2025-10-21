58e8d4c8221e20ada11b8f5f0780d3f04aad7e80
Quien <quien697@gmail.com>
2025-10-21 23:48:55 +0800

    feat: complete "Drawing paths and shapes" section from Landmarks tutorial

---
+++ Landmarks/Views/Badges/RotatedBadgeSymbol.swift
@@ -0,0 +1,22 @@
+//
+//  RotatedBadgeSymbol.swift
+//  Landmarks
+//
+//  Created by Quien on 2025-10-21.
+//
+
+import SwiftUI
+
+struct RotatedBadgeSymbol: View {
+  let angle: Angle
+  
+  var body: some View {
+    BadgeSymbol()
+      .padding(-60)
+      .rotationEffect(angle, anchor: .bottom)
+  }
+}
+
+#Preview {
+  RotatedBadgeSymbol(angle: Angle(degrees: 5))
+}
