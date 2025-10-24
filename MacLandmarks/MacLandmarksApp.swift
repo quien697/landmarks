6c00e2dbff17f7f3ff46fbc0576be82964c37c1f
Quien <quien697@gmail.com>
2025-10-24 17:52:31 +0800

    feat: add initial macOS target

---
+++ MacLandmarks/MacLandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacLandmarksApp.swift
+//  MacLandmarks
+//
+//  Created by Quien on 2025-10-24.
+//
+
+import SwiftUI
+
+@main
+struct MacLandmarksApp: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
---
+++ MacLandmarksTests/MacLandmarksTests.swift
@@ -0,0 +1,17 @@
+//
+//  MacLandmarksTests.swift
+//  MacLandmarksTests
+//
+//  Created by Quien on 2025-10-24.
+//
+
+import Testing
+@testable import MacLandmarks
+
+struct MacLandmarksTests {
+  
+  @Test func example() async throws {
+    // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+  }
+  
+}
