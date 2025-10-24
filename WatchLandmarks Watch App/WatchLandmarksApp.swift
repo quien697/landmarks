c5d41545c675fba59ad8682fd264c23657bcd5a5
Quien <quien697@gmail.com>
2025-10-24 15:59:11 +0800

    feat: add initial watchOS target

---
+++ WatchLandmarks Watch App/WatchLandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchLandmarksApp.swift
+//  WatchLandmarks Watch App
+//
+//  Created by Quien on 2025-10-24.
+//
+
+import SwiftUI
+
+@main
+struct WatchLandmarks_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WatchLandmarks Watch AppTests/WatchLandmarks_Watch_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  WatchLandmarks_Watch_AppTests.swift
+//  WatchLandmarks Watch AppTests
+//
+//  Created by Quien on 2025-10-24.
+//
+
+import Testing
+@testable import WatchLandmarks_Watch_App
+
+struct WatchLandmarks_Watch_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
